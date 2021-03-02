161f126a82994b7af34f73d025e082a7b8452dd5
vmwhoami <vmwhoami@gmail.com>
2021-03-02 17:00:49 +0200

    add input and select option to the local state

--- src/components/ BooksForm.js
+++ src/components/ BooksForm.js
@@ -1,15 +1,44 @@
+import React, { useState } from 'react';
+
 const BooksForm = () => {
   const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
-  return (
-    <form>
+  const [title, setTitle] = useState('');
+  const [catValue, setCatValue] = useState('');
 
-      <input type="text" id="title" name="title" placeholder="Title" />
-      <select>
+  const handleChange = e => {
+    if (e.target.name === 'title') {
+      setTitle(e.target.value);
+    } else {
+      setCatValue(e.target.value);
+    }
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+  };
+
+  return (
+    <form onSubmit={handleSubmit} name="form">
+
+      <input
+        type="text"
+        id="title"
+        name="title"
+        placeholder="Title"
+        onChange={e => handleChange(e)}
+        value={title}
+
+      />
+      <select onChange={e => handleChange(e)}>
         {categories.map(category => (
-          <option value={category} key={category}>{category}</option>
+          <option value={category} key={category}>
+            {category}
+          </option>
         ))}
       </select>
       <button type="submit">Submit</button>
+      <h4>{title}</h4>
+      <h2>{catValue}</h2>
     </form>
   );
 };
