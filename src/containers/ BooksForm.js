510484a2462693d7321c386c64bb902f0b9c06ee
haraphat01 <haraphat@gmail.com>
2021-03-03 09:56:06 +0100

    set up new working branch

--- src/containers/ BooksForm.js
+++ src/containers/ BooksForm.js
@@ -1,17 +1,52 @@
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { addBook } from '../actions ';
+
 const BooksForm = () => {
   const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
-  return (
-    <form>
+  const [title, setTitle] = useState('');
+  const [catValue, setCatValue] = useState('');
 
-      <input type="text" id="title" name="title" placeholder="Title" />
-      <select>
+  const handleChange = e => {
+    if (e.target.name === 'title') {
+      setTitle(e.target.value);
+    } else {
+      setCatValue(e.target.value);
+    }
+    // const obj = { title, category: catValue };
+    // dispatch(addBook(obj));
+    props.store.dispatch(addBook());
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+  };
+
+  return (
+    <form onSubmit={handleSubmit} name="form">
+
+      <input
+        type="text"
+        id="title"
+        name="title"
+        placeholder="Title"
+        onChange={e => handleChange(e)}
+        value={title}
+
+      />
+      <select onChange={e => handleChange(e)}>
         {categories.map(category => (
-          <option value={category} key={category}>{category}</option>
+          <option value={category} key={category}>
+            {category}
+          </option>
         ))}
       </select>
       <button type="submit">Submit</button>
+      <h4>{title}</h4>
+      <h2>{catValue}</h2>
     </form>
   );
 };
+const mapStateToProps = state => state;
 
-export default BooksForm;
+export default connect(mapStateToProps)(BooksForm);
