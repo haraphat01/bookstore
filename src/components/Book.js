c0bab6e9bae16d7d9b4b82624475362cae1808e8
vmwhoami <vmwhoami@gmail.com>
2021-03-05 16:08:17 +0200

    Styled the form section

--- src/components/Book.js
+++ src/components/Book.js
@@ -2,15 +2,19 @@ import PropTypes from 'prop-types';
 
 const Book = ({ book, handleRemoveBook }) => {
   const { id, title, category } = book;
-
+  const names = ['William Shakespeare', 'Emily Dickinson', 'Arthur Conan Doyle', 'Leo Tolstoy', 'Edgar Allan Poe', 'Robert Ervin Howard'];
+  const authors = arr => names[Math.floor(Math.random() * arr.length)];
   return (
     <article className="book">
+
       <div className="bookInfo">
+
         <div className="catTitAuth">
           <p>{category}</p>
           <h2>{title}</h2>
-          <h4>Author</h4>
+          <h4>{authors(names)}</h4>
         </div>
+
         <div className="bookBtns">
           <button type="button">Comment</button>
           <button type="button" onClick={() => handleRemoveBook(id)}>Remove</button>
@@ -19,8 +23,8 @@ const Book = ({ book, handleRemoveBook }) => {
 
         <div className="progress">
           <div className="circle" />
-
         </div>
+
       </div>
 
     </article>
