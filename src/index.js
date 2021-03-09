e23727cfc40db5f1c984e0dd867a54490f7cc983
haraphat01 <haraphat@gmail.com>
2021-03-09 16:39:19 +0100

    crated the new branch

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { createStore } from 'redux';
@@ -20,17 +20,38 @@ const initial = {
       title: 'Book of heaven',
       category: 'Fiction',
     },
-
   ],
-
-  filter: 'All',
 };
+const
+useEffect(()=>{ 
+test()
+},[])
+const test = async () => {
+  const url = 'https://bookstoreapiakata.herokuapp.com/books';
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: 'GET', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json',
+    // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    // body data type must match "Content-Type" header
+  });
+  const data = await response.json();
+  return data
+
+};
+test();
 /* eslint-disable no-underscore-dangle */
 const store = createStore(
   rootReducer, initial,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
-
 /* eslint-enable */
 ReactDOM.render(
   <React.StrictMode>
